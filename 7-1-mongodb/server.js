338815b6f9ab7d2c6b2569fb84b22336005ee6dc
Zahramahdi32 <s202271780@kfupm.edu.sa>
2025-11-17 12:20:36 +0300

    Done

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -170,22 +170,61 @@
 import mongoose from "mongoose";
 
 // establish connection
+const uri =
+  "mongodb+srv://s202271780:Mfhhzaa.1388@cluster0.hb6hpwb.mongodb.net/";
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("❌ Connection error:", err);
+  });
 
 
 // define schema
+const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  major: String,
+});
+
+const Student = mongoose.model("Student", studentSchema);
 
 
 // create document
-
+async function createStudents() {
+  await Student.insertMany([
+    { name: "Ali", age: 21, major: "CS" },
+    { name: "Sara", age: 23, major: "SE" },
+  ]);
+  console.log("✅ Inserted");
+}
 
 // read document
 
+async function readStudents() {
+  const all = await Student.find();
+  console.log("📚 All students:", all);
+}
 
 // update document
-
+async function updateStudent() {
+  await Student.updateOne({ name: "Ali" }, { age: 22 });
+  console.log("✅ Updated Ali");
+}
 
 // delete document
+async function deleteStudent() {
+  await Student.deleteOne({ name: "Sara" });
+  console.log("✅ Deleted Sara");
+}
 
+//createStudents();
+// readStudents();
+//updateStudent();
+ deleteStudent();
 
 
 
